d8e42721193a2592605b6335c4d5a6c1679a0ee0
jseiglie <javierseiglie@gmail.com>
2025-05-03 14:02:17 +0200

    feat: basic footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='text-white mt-5'>
+      <div className="container d-flex justify-content-center align-items-center flex-column">
+        <p className='text-center'>© 2025 BandAid. All rights reserved.</p>
+        <p className='text-center'>Made with ❤️ by the BandAid team</p>
+      </div>
+    </footer>
   )
 }
