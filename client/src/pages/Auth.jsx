b05f06638a2545d0995732de55c1b17a4f748365
jseiglie <javierseiglie@gmail.com>
2025-07-24 12:31:45 +0200

    style: style root

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -4,14 +4,15 @@ import { Authenticate } from "../components/authenticate/authenticate";
 export const Auth = () => {
   const [login, setLogin] = useState(true);
   return (
-    <section className="vh-100 text-primary">
+    <section className="vh-100 text-primary container d-flex flex-column justify-content-center align-items-center">
+      
       <Authenticate login={login} />
       <div className="d-flex justify-content-center align-items-center">
-        <p className="text-white d-flex align-items-center">
+        <p className=" d-flex align-items-center text-black">
           {" "}
           {login ? "In need of an account? " : "Already have an account? "}
           <span
-            className="btn btn-outline text-white fw-bolder"
+            className="btn btn-outline  fw-bolder"
             onClick={() => setLogin(!login)}
           >
             {login ? "Register" : "Login"}
